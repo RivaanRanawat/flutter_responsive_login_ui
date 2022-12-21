fe51891880395e02caa8ee942a1a785403c53ebe
Rivaan Ranawat <namanrivaan@gmail.com>
2022-12-21 22:39:19 +0530

    adding entire project

---
+++ lib/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/widgets/gradient_button.dart';
+import 'package:flutter_responsive_login_ui/widgets/login_field.dart';
+import 'package:flutter_responsive_login_ui/widgets/social_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                'Sign in.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const SocialButton(iconPath: 'assets/svgs/g_logo.svg', label: 'Continue with Google'),
+              const SizedBox(height: 20),
+              const SocialButton(
+                iconPath: 'assets/svgs/f_logo.svg',
+                label: 'Continue with Facebook',
+                horizontalPadding: 90,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'or',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(height: 15),
+              const LoginField(hintText: 'Email'),
+              const SizedBox(height: 15),
+              const LoginField(hintText: 'Password'),
+              const SizedBox(height: 20),
+              const GradientButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/login_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/pallete.dart';
+
+class LoginField extends StatelessWidget {
+  final String hintText;
+  const LoginField({
+    Key? key,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxWidth: 400,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(27),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Pallete.borderColor,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Pallete.gradient2,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
